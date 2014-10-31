701bee053b43c8283190ef9a42de695318868ca2
Sebastien Diemer <sebastien.diemer@gmail.com>
2014-10-31 17:57:21 +0100

    Added a slider to adjust the SPL threshold live
    I added a Qt slider, and linked the signal `valueChanged` to the variable
    SPL_THRESHOLD which fixes the frequencies to be displayed.

--- pyaudio_seb.py
+++ pyaudio_seb.py
@@ -14,19 +14,21 @@ CHUNK_SIZE = 2**9 # size of the chunk audio data acquired in one call
                   # 2**9 = about 0.01 s of recording
 
 # DFT parameters
-WINDOW_SIZE = 2**13 # size of the data on which the DFT operates
-WINDOW_OVERLAP = 0.2
+WINDOW_SIZE = 2**11 # size of the data on which the DFT operates
+#WINDOW_OVERLAP = 0.2
 
-BUFFER_SIZE = int(WINDOW_SIZE*(1 + WINDOW_OVERLAP))
+#BUFFER_SIZE = int(WINDOW_SIZE*(1 + WINDOW_OVERLAP))
+BUFFER_SIZE = WINDOW_SIZE
 
 # Global variables
 BUFFER = deque(np.zeros(BUFFER_SIZE), maxlen=BUFFER_SIZE) # audio data history
 FOURIER_SPL = [] # SPL = Signal Power Level : data to be displayed
+SPL_THRESHOLD = 0. # we do not display frequencies whose SPL is below this limit
 
 # pyqtgraph initialization
 app = QtGui.QApplication([])
 
-win = pg.GraphicsWindow(title="Basic plotting examples")
+win = QtGui.QMainWindow()
 win.resize(1000,600)
 win.setWindowTitle('pyqtgraph example: Plotting')
 
@@ -38,8 +40,9 @@ p = pyaudio.PyAudio()
 def callback(in_data, frame_count, time_info, status):
     data = np.fromstring(in_data, np.float32)
     BUFFER.extend(data)
-    process_data(data[:WINDOW_SIZE])
-    return (None, pyaudio.paContinue)
+    return (None, pyaudio.paContinue) # When using pyaudio in input-only, the
+                                      # callback should return None as first
+                                      # element
 
 def process_data(data):
     global FOURIER_SPL
@@ -98,28 +101,41 @@ stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=RATE,
                 input=True,
-                frames_per_buffer=WINDOW_SIZE,
+                frames_per_buffer=CHUNK_SIZE,
                 stream_callback=callback)
 
-p6 = win.addPlot(title="Updating plot")
+lw = pg.LayoutWidget()
+p6 = pg.PlotWidget(name="Spiral")
+lw.addWidget(p6)
+slider = QtGui.QSlider(orientation=QtCore.Qt.Horizontal)
+slider.setMaximumWidth(100)
+def test(i):
+    global SPL_THRESHOLD
+    SPL_THRESHOLD = i - 50.
+    
+slider.valueChanged.connect(test)
+lw.addWidget(slider)
+#p6 = win.addPlot(title="Updating plot")
 p6.setAspectLocked()
-curve = p6.plot(pen='y')
 ptr = 0
 
 MAX_NB_CURVES = 100
 CURVES = [QtGui.QGraphicsLineItem() for _ in xrange(MAX_NB_CURVES)]
 for c in CURVES:
     p6.addItem(c)
-    #c.setZValue(100)
-    c.setPen(pg.mkPen('g'))
+    c.setPen(pg.mkPen(color=(255, 255, 128)))
 
 def draw_spiral(r_max=8., r_scale=1.):
     r = np.linspace(0, r_max, 1000)
     theta = 2*np.pi*r*r_scale
-    p6.plot(r*np.cos(theta), r*np.sin(theta), pen='r')
+    p6.plot(r*np.cos(theta), r*np.sin(theta), pen=pg.mkPen(color=(128, 64, 64)))
     for note, freq in BASE_FREQUENCIES.items():
         _, theta = get_polar_coordinates(freq)
-        p6.plot([0, r_max*np.cos(theta)], [0, r_max*np.sin(theta)], pen='b')
+        p6.plot([0, r_max*np.cos(theta)], [0, r_max*np.sin(theta)],
+                pen=pg.mkPen(color=(128, 64, 64), style=QtCore.Qt.DotLine))
+        text = pg.TextItem(note, anchor=(0.5, 0.5))
+        text.setPos((r_max + 1.)*np.cos(theta), (r_max + 1.)*np.sin(theta))
+        p6.addItem(text)
 
 def blob_data(freq, intensity, r_scale=1.):
     r, theta = get_polar_coordinates(freq, r_scale=r_scale)
@@ -129,14 +145,14 @@ def blob_data(freq, intensity, r_scale=1.):
             r_max*np.cos(theta), r_max*np.sin(theta))
 
 def update():
-    global CURVES
-    threshold = 0.
+    global CURVES, SPL_THRESHOLD
+    process_data(BUFFER)
     ranking = sorted([(spl, i) for i, spl in enumerate(FOURIER_SPL)],
                      reverse=True)[:MAX_NB_CURVES]
     frequencies = np.fft.rfftfreq(WINDOW_SIZE, 1./RATE)
     for j, el in enumerate(ranking):
         spl, i = el
-        if spl > threshold:
+        if spl > SPL_THRESHOLD:
             freq = frequencies[i]
             blob = blob_data(freq, spl)
             CURVES[j].setLine(*blob)
@@ -147,5 +163,7 @@ draw_spiral()
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(50)
+timer.start(20)
+
+lw.show()
 QtGui.QApplication.instance().exec_()
