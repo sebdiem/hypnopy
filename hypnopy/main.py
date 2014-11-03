d3b20ada21e1cccbb0bee74ec2d5f5e669806d82
Sébastien Diemer <diemersebastien@yahoo.fr>
2014-11-03 14:38:36 +0100

    added a new option to set the window size + refactoring

--- hypnopy/main.py
+++ hypnopy/main.py
@@ -3,13 +3,18 @@ import argparse
 from gui import launch_gui
 from audio import start_acquisition
 
-if __name__ == '__main__':
+def parse_args():
     parser = argparse.ArgumentParser(description='hypnopy: a real-time sound vizualizer')
     parser.add_argument('--freq', nargs=1, type=float, metavar='f', dest='f',
                         help='Test gui with a pure tone of frequency f')
-    parser.add_argument('--blob_count', nargs=1, type=int, metavar='N', dest='blobs',
+    parser.add_argument('--blobs', nargs=1, type=int, metavar='N', dest='blobs',
                         help='Sets the number of displayed blobs to N (default 100)')
-    args = parser.parse_args()
+    parser.add_argument('--window', nargs=1, type=int, metavar='N', dest='window',
+                        help='Sets the window size used to compute the sound intensity to N (default 8192)')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
     p = start_acquisition()
     freq = None if not args.f else args.f[0]
     blobs = 100 if not args.blobs else args.blobs[0]
