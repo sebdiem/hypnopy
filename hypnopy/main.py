92045141c9da6ddc1790b02e605e6532a4974147
Sébastien Diemer <diemersebastien@yahoo.fr>
2014-11-03 13:36:04 +0100

    added command line arguments parsing, and a new option to test the gui with a pure tone of given frequency

--- hypnopy/main.py
+++ hypnopy/main.py
@@ -1,7 +1,14 @@
+import argparse
+
 from gui import launch_gui
 from audio import start_acquisition
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='hypnopy: a real-time sound vizualizer')
+    parser.add_argument('--freq', nargs=1, type=float, metavar='f', dest='f',
+                        help='Test gui with a pure tone of frequency f')
+    args = parser.parse_args()
     p = start_acquisition()
-    launch_gui()
+    freq = None if not args.f else args.f[0]
+    launch_gui(freq_test=freq)
 
